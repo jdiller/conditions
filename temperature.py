ca09183c77390c86deb75725b02874ef03541c8a
Jason Diller <jason@diller.ca>
2016-02-15 15:18:31 -0500

    Improve the reliability of sensor reads. Add retries w/backoff and pause polling on blur. Make GPIO interface object global.

--- temperature.py
+++ temperature.py
@@ -15,15 +15,15 @@ r = redis.StrictRedis(host='localhost', port=6379, db=0)
 Conditions = namedtuple('Conditions', 'temperature humidity message')
 logging.basicConfig(level=logging.DEBUG)
 
+pi = pigpio.pi()
+CACHE_TIME = 5
 
 @app.route("/")
 def home():
     return render_template('main.html')
 
-
 @app.route("/current")
 def conditions():
-    pi = pigpio.pi()
     last_reading_str = r.get('last_reading')
     if last_reading_str:
         try:
@@ -35,11 +35,18 @@ def conditions():
             r.delete('last_reading')
     else:
         last_reading = None
-    if not last_reading or time.time() - last_reading[3] > 5:
+    if not last_reading or time.time() - last_reading[3] > CACHE_TIME:
         logging.debug("Getting new readings")
         sensor = Sensor(pi, 25)
         sensor.read()
-        time.sleep(0.3)
+        RETRIES = 4
+        sleep = 0.1
+        time.sleep(sleep)
+        times = 0
+        while times < RETRIES and sensor.temperature == -999:
+            sleep = sleep * 2
+            time.sleep(sleep)
+            times += 1
         r.set('last_reading', (sensor.temperature,
                                sensor.humidity, sensor.message, time.time()))
         conditions = Conditions(
